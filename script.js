90f828a85217ee07e27c462eb67f9a31cb6a3455
Daricio <140209305+Daricio@users.noreply.github.com>
2023-08-11 01:32:01 +0300

    Implement play single round function

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ function getComputerChoice() {
     // if 3 - return scissors
     switch (choice) {
         case 1:
-            return 'Rock';
+            return 'rock';
             break;
 
         case 2:
-            return 'Paper';
+            return 'paper';
             break;
 
         case 3:
-            return 'Scissors';
+            return 'scissors';
             break;
 
         default:
@@ -34,4 +34,36 @@ function generateRandom(min, max) {
     // add min
     rand += min;
     return rand;
+}
+
+// function that plays a single round of Rock Paper Scissors. 
+// The function should take two parameters - the playerSelection and computerSelection 
+// - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+
+function playRound(playerSelection, computerSelection) {
+    // capitalize player celection
+    playerSelection = capitalize(playerSelection);
+    // if player = computer -> tie; print playerChoice vs. Computer Choice
+    if (playerSelection === computerSelection) {
+        return `A tie! ${playerSelection} vs. ${computerSelection}`;
+    }
+    // player - rock, computer - paper -> you lose
+    // player - paper, computer - scissors -> you lose
+    // player - scissors, computer - rock -> you lose, Computer Choice beats Player Choice
+    else if (playerSelection === 'Rock' && computerSelection === 'Paper'
+    || playerSelection === 'Paper' && computerSelection === 'Scissors'
+    || playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    // otherwise player win, Player Choice beats Computer Choice
+    else {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+}
+
+function capitalize(string) {
+    string = string.toLowerCase();
+    firstLetter = string.substr(0, 1);
+    string = string.replace(firstLetter, firstLetter.toUpperCase());
+    return string;
 }
